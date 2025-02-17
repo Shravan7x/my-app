0aad1223f7d879f5862072c0c97299b6c596fc2e
Shravan7x <Shravanp50rj3@gmail.com>
2025-02-17 22:25:02 +0530

    modules in js

---
+++ module1.mjs
@@ -0,0 +1,2 @@
+import dz, {a, c, d} from "./module2.mjs";
+console.log(dz, a, c, d);
---
+++ module2.mjs
@@ -0,0 +1,7 @@
+const a = "sharavn";
+const b = "Anaya";
+const c = "Priyanka";
+const d = "Neha"; 
+
+export default b;
+export {a, c, d};
